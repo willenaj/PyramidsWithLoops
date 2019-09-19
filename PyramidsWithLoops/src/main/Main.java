e0eca360f030ba876722401e3e856f2924a69013
willenaj <willenaj@LCBLAB2110-37.uccob.uc.edu>
2019-09-19 09:19:16 -0400

    WIP

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -1,6 +1,6 @@
 /*
  * Alex Willenborg
- * nicholdw@ucmail.uc.edu
+ * willenaj@mail.uc.edu
  * Printing pyramids using loops. It kind-of works. 
  * What I really want is
         1
@@ -11,15 +11,31 @@
 */
 package main; 
 
+
 public class Main {
 
 	public static void main(String[] args) {
-		int lines = 5;
-		for (int i = 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
-				System.out.print(j);
+		int lines = 6;
+		
+		
+		for (int i = 1; i < lines; i++) {
+			for (int j = 1; j < i+1; j++) {
+				//char s = 0;
+				//char t = 0;
+				//String repeated = new String(new char[j]).replace(t, s);
+				//String str = j;
+				//String repeated = str.repeat(3);
+				//System.out.print(j);
+				int n = 1;
+			    for (int u=1; u<=n; u++) {
+			    System.out.println(new String(new char[i]).replace("\0", n+""));
+			    n++;
+			    if(n == 5) {
+			    	break;
+			    }
+			    }
 			}
-			System.out.println("");
+			//System.out.println("");
 		}
 	}
 }
