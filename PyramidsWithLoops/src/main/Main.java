4673001cb37ed0e82635b37b9e8b38b222df7f89
Alex Willenborg <Alex Willenborg@DESKTOP-8ESBUE0.lan1>
2019-09-25 00:24:13 -0400

    Finishing assignment 4

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -15,27 +15,32 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		int lines = 6;
-		
-		
-		for (int i = 1; i < lines; i++) {
-			for (int j = 1; j < i+1; j++) {
-				//char s = 0;
-				//char t = 0;
-				//String repeated = new String(new char[j]).replace(t, s);
-				//String str = j;
-				//String repeated = str.repeat(3);
-				//System.out.print(j);
-				int n = 1;
-			    for (int u=1; u<=n; u++) {
-			    System.out.println(new String(new char[i]).replace("\0", n+""));
-			    n++;
-			    if(n == 5) {
-			    	break;
-			    }
-			    }
-			}
-			//System.out.println("");
-		}
-	}
+		//setting the amount of rows i want
+		int rows = 5;
+		//setting how many numbers i want per row
+        int count = 1;
+        //setting the actual number printed
+        int number = 1;
+       //stopping the loop if a is less than 0 and subtracting one every iteration
+        for (int a = rows; a > 0;a--)
+        {
+            //Printing spaces at the beginning of each row
+            for (int j = 1; j <= a;j++ )
+            {
+                System.out.print(" ");
+            }
+            //printing the numbers and the amount of numbers
+           
+            for (int j = 1; j <= count; j++)
+            {
+                System.out.print(number);            
+            }
+            System.out.println();
+             //Incrementing the numbers per row and the actual number in the row
+            count+=2;
+            number++;
+        }
+    }
+    
+	
 }
